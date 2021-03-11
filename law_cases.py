a363d7257e63599ef53207817551dc637557d700
itb-ie <42895988+itb-ie@users.noreply.github.com>
2021-03-11 14:39:41 +0100

    Added more explanations

--- law_cases.py
+++ law_cases.py
@@ -2,8 +2,14 @@ import requests
 # this is a library that we use to pass http requests
 # an http request is what a web browsers sends to a web server and then displays the information on screen
 
+
 # there are multiple pages potentially, because a search only includes 100 cases per page. So we open all the links and save each page.
 def get_pages(url):
+    """
+    In case of many query results, they will be on more than one page
+    :param url: the url to search
+    :return: a list of all the pages
+    """
     page_list = []
     while True:
         # this is the only place in which we do a request and get the page from the online website case.law
@@ -26,6 +32,11 @@ def get_pages(url):
 
 # iterate through all the pages and get all the cases
 def get_cases(url):
+    """
+    Get all the cases for the construted url
+    :param url: the case.law search query url
+    :return: a list of all the cases
+    """
     case_list = []
     # call the previous function to get all the URL pages
     page_list = get_pages(url)
@@ -38,6 +49,11 @@ def get_cases(url):
 
 
 def search_story(keyword):
+    """
+    This is the function that searches for a story and returns the olderst found case
+    :param keyword: the keyword(s) to search for
+    :return: prints the link on case.law for that particular oldest story
+    """
     starting_url = 'https://api.case.law/v1/cases/?search=' + keyword
 
     # call previous function to get all the cases for the starting_url that is constructed above
@@ -46,13 +62,13 @@ def search_story(keyword):
     results_count = len(case_list)
     # go through each case
     for case in case_list:
-
         id = case['id']
         name = case['name_abbreviation']
         jurisdiction = case['jurisdiction']['name_long']
         date = case['decision_date']
         url = case['url']
         if ("date" not in oldest_case) or (oldest_case["date"] > date):
+            # this is the oldest case so far
             oldest_case["id"] = id
             oldest_case["jurisdiction"] = jurisdiction
             oldest_case["date"] = date
@@ -65,7 +81,7 @@ def search_story(keyword):
 # here you enter the keyword to be searched. Use %20 for SPACE character
 print("Please wait, this could take a while depending on the query performed")
 # Here you can change the query that you want to do
-# search_story('El%20Chapo%20Guzman')
-search_story('"Mike%20Tyson"')
+search_story('El%20Chapo%20Guzman')
+# search_story('"Mike%20Tyson"')
 
 print("And we are all done")
